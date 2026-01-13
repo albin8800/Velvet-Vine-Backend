a6ceee23a62c16e6967d50c21fff813120fc621c
Albin B Thomas <albinbthomas8800@gmail.com>
2026-01-13 19:40:40 +0530

    fix : backend

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,3 +1,7 @@
+
+
+
+
 import Order from "../models/Order.js";
 import User from "../models/User.js";
 
@@ -49,3 +53,44 @@ const createOrder = async (req, res) => {
         });
     }
 };
+
+
+// (User)
+const getMyOrders = async (req, res) => {
+    try {
+        const orders = await Order.find({ 
+            user: req.user._id
+        })
+        .populate('items.product', 'name price');
+
+        res.json({
+            success: true,
+            orders
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+}
+
+// (admin)
+
+const getAllOrders = async (req, res) => {
+    try {
+        const orders = await Order.find()
+        .populate("user", "email role")
+        .populate("items.product", "name price");
+
+        res.json({
+            success: true,
+            orders
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+}
+
+export { createOrder, getMyOrders, getAllOrders };
